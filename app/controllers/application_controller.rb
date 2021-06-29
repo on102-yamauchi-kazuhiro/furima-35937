39e10794b89fb4d606969783b168ce8dd82975f7
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-06-29 17:00:30 +0900

    コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
   def basic_auth
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
       username == 'furima-admin' && password == '35937'
     end
   end
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 end
