d8ce75a41e06ab1a122eaf48780f9076c20cab5c
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-06-29 14:56:32 +0900

    新規登録ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # localhost:3000にアクセスしたときにitemsコントローラーのindexアクションをうごかすための記述を書く。
-  # devise_for :users
   root to: 'items#index'
 end
