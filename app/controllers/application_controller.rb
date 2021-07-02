85d5333789335ba9d7df44d1feecfea3e61e2644
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-02 11:49:00 +0900

    ユーザーテーブルにカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!
   
   private
   def basic_auth
@@ -9,6 +10,12 @@ class ApplicationController < ActionController::Base
     end
   end
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date])
   end
 end
