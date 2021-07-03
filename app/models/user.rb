6887c032ff6c1f1c657a96f0a18081de75f84069
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-03 12:11:21 +0900

    テストコードの実装２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :user_name, presence: true, length: { maximum: 6 }
+         
+  validates :user_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX,
+            message: "must include both letters and numbers"}
 end
