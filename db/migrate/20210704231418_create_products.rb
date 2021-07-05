59fa0c5329bd7003ec2db3f25d63dbdc34b78f63
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-05 11:09:25 +0900

    Productテーブルの作成

---
+++ db/migrate/20210704231418_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :product_name,       null: false
+      t.text :description,          null: false
+      t.integer :condition_id,      null: false
+      t.integer :price,             null: false
+      t.integer :trading_status_id, null: false
+      t.integer :category_id,       null: false
+      t.integer :shipping_fee_id,   null: false
+      t.integer :prefecture_id,     null: false
+      t.references :user,           foreign_key: true
+      t.timestamps
+    end
+  end
+end
