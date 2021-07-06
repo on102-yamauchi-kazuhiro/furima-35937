d4d52a09bcafaacce0bf734dcf52de329e7ed16d
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-06 23:24:49 +0900

    テストコードの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,18 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :product_name
     validates :description
-    validates :condition_id
-    validates :trading_status_id
+    validates :user
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be in between ¥300 to ¥9,999,999" }
+  end
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
+    validates :condition_id
     validates :shipping_fee_id
     validates :prefecture_id
-    validates :user
+    validates :trading_status_id
   end
 end
