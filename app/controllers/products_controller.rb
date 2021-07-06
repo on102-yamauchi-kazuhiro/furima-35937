a5039835444e9f621dd8c430fbe59a15b28ca21c
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-06 15:19:00 +0900

    商品登録の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,24 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.includes(:user)
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
   def product_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:product_name, :content, :image, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :trading_status_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :condition_id
+    validates :trading_status_id
+    validates :category_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :user
+  end
 end
