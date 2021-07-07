9bbd1ff60444d04d2d9631f5a1da752ea95edec9
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-07 15:27:00 +0900

    商品出品機能 - 修正2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  
   def index
     @products = Product.includes(:user)
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -107,6 +107,11 @@ RSpec.describe Product, type: :model do
         @product.valid?
         expect(@product.errors.full_messages).to include('Price is not a number')
       end
+      it 'ユーザーが紐づいていなければ投稿できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
