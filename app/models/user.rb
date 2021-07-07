91a4cfa9dd0f9fc055bc6a607cdfed479e241035
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-07 16:39:09 +0900

    商品出品機能 - 修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  association :product
+  has_many :products
 
   with_options presence: true do
     validates :user_name
