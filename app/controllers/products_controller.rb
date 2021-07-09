19ab9dedfebaccea5fa25515f9746923ee507228
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-09 18:01:30 +0900

    「もどる」を押下した際にその商品詳細ページへ遷移

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   extend ActiveHash::Associations::ActiveRecordExtensions
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :product, only: [:edit, :show]
+  before_action :product, only: [:edit, :show, :update]
   
   def index
     @products = Product.includes(:user).order("created_at DESC")
@@ -20,21 +20,32 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:product_name, :image, :description, :category_id, :condition_id, :shipping_fee_id,
-                                    :prefecture_id, :trading_status_id, :price).merge(user_id: current_user.id)
-  end
-
   def edit
     @product = Product.find(params[:id])
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def show
   end
 
   # def destroy
   #   product = Product.find(params[:id])
   #   product.destroy
   # end
+  
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :image, :description, :category_id, :condition_id, :shipping_fee_id,
+                                    :prefecture_id, :trading_status_id, :price).merge(user_id: current_user.id)
+  end
 
   def product
     @product = Product.find(params[:id])
