f421b3097b3d9e593d5f3b47782b2c0456529b2d
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-14 18:21:12 +0900

    エラーメッセージの日本語化

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,11 +5,11 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :product_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-).' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は、ハイフン（-）を入れて入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Please enter 10 or 11 digit number.' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は、１０桁または１１桁で入力してください' }
     validates :token
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
     validates :birth_date
   end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'は全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'は全角カタカナで入力してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/i
   with_options presence: true,
                format: { with: VALID_PASSWORD_REGEX,
-                         message: 'must include both letters and numbers (half-width)' } do
+                         message: 'は半角英数字で入力してください' } do
     validates :password
   end
 end
