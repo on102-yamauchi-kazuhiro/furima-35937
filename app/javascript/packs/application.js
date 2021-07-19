7e212d7312f65b574002dc8fda5994daad264376
on102-yamauchi-kazuhiro <85274620+on102-yamauchi-kazuhiro@users.noreply.github.com>
2021-07-19 18:16:05 +0900

    投稿する画像のプレビュー表示の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../credit_card")
 
-require("../product_price");
+require("../credit_card")
+require("../product_price")
+require('../preview')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
---
+++ app/javascript/preview.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+  if ( document.getElementById('product-image')){
+    const ImageList = document.getElementById('image-list');
+    console.log(ImageList)
+    document.getElementById('product-image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('image-list');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+      // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('height', 100)
+      blobImage.setAttribute('class', 'image-list')
+
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+
+      createImageHTML(blob);
+    });
+  }
+});
